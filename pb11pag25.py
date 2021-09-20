54aa94d1994b76a5e7b82d693922b019610cfef0
Savca Mihaela <70644060+SavcaMihaela@users.noreply.github.com>
2021-09-21 00:03:48 +0300

    Delete pb11pag25.py

--- pb11pag25.py
+++
@@ -1,8 +0,0 @@
-n=int(input('Dati numarul de elemente din vector='))
-a=[]
-print('Introduceti ',n,'elemente pentru vectorul creat')
-for i in range(0, n):
-    x=int(input('Dati elementul='))
-    a.extend([x])
-print(a)
-
